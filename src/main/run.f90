63637336c21a4ce9b7ff6be236c16aaef7edca16
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-03-10 12:47:23 +0100

    move main prefixed files to main directory

---
+++ src/main/run.f90
@@ -0,0 +1,28 @@
+! This file is part of xtb.
+!
+! Copyright (C) 2019-2020 Sebastian Ehlert
+!
+! xtb is free software: you can redistribute it and/or modify it under
+! the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! xtb is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU Lesser General Public License for more details.
+!
+! You should have received a copy of the GNU Lesser General Public License
+! along with xtb.  If not, see <https://www.gnu.org/licenses/>.
+
+!> TODO
+module xtb__
+   use xtb_mctc_accuracy, only : wp
+   implicit none
+   private
+
+
+contains
+
+
+end module xtb__
