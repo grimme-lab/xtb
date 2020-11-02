514580a65b9208596620dfb20d9d9987e74f2a85
MtoLStoN <70513124+MtoLStoN@users.noreply.github.com>
2020-11-02 10:48:49 +0100

    Fixed problems with the optimization of large proteins.
    Signed-off-by: MtoLStoN <70513124+MtoLStoN@users.noreply.github.com>

--- TESTSUITE/gfnff.f90
+++ TESTSUITE/gfnff.f90
@@ -608,9 +608,9 @@ subroutine test_gfnff_sdf
    real(wp) :: energy, hl_gap, sigma(3, 3)
    real(wp), allocatable :: gradient(:, :)
    real(wp), parameter :: ref_energies(3) = &
-      &[-0.98330642628373_wp, -1.0751892050077_wp, -1.0752776712286_wp]
+      &[-0.98330642628373_wp, -1.0769327949979_wp, -1.0770116012984_wp]
    real(wp), parameter :: ref_gnorms(3) = &
-      &[0.11515550863614e-2_wp, 0.47224675049592e-2_wp, 0.47021127890168e-2_wp]
+      &[0.11515550863614e-2_wp, 0.49692321804809e-2_wp, 0.49548841785655e-2_wp]
 
    call init(env)
    do iMol = 1, 3
