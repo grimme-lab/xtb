bfd7d39f6ca5f9739044842f4f07b6bd7ff3dc59
FulgurIgor <i.s.ger@ya.ru>
2020-11-06 16:08:20 +0300

    Fix all warnings in tmgrad.f90
    Signed-off-by: FulgurIgor <i.s.ger@ya.ru>

--- src/tmgrad.f90
+++ src/tmgrad.f90
@@ -38,22 +38,12 @@ subroutine tmgrad(nat,iat,xyz,g,edum)
    etemp = get_namespace('energy.tmp')
    open(newunit=ien,file=efile)
    open(newunit=jen,file=etemp)
-   read(ien,'(a)',end=33)a1
-33 continue
+   read(ien,'(a)')a1
    write(jen,'(a)')a1
-20 read(ien,'(a)',end=44)a
-   if(index(a,'$').eq.0)then
-      write(jen,'(a)')a
-      call readl(a,xx,nn)
-      j=idint(xx(1))
-   else
-   44 write(jen,'(i6,3F18.11)')j+1,edum,edum,edum
-      a='$end'
-      write(jen,'(a)')a
-      goto 22
-   endif
-   goto 20
-22 continue
+   read(ien,'(a)')a
+   write(jen,'(i6,3F18.11)')j+1,edum,edum,edum
+   a='$end'
+   write(jen,'(a)')a
    close(ien)
    close(jen)
    call execute_command_line('mv '//etemp//' '//efile)
@@ -66,30 +56,20 @@ subroutine tmgrad(nat,iat,xyz,g,edum)
    gtemp = get_namespace('gradient.tmp')
    open(newunit=igr,file=gfile)
    open(newunit=jgr,file=gtemp)
-   read(igr,'(a)',end=31)a1
-31 continue
+   read(igr,'(a)')a1
    write(jgr,'(a)')a1
-21 read(igr,'(a)',end=41)a
-   if(index(a,'$').eq.0)then
-      write(jgr,'(a)')a
-      call readl(a,xx,nn)
-      if(index(a,'cycle').ne.0)j=idint(xx(1))
-   else
-   41 write(jgr,'(''  cycle = '',i6,4x,''SCF energy ='',F18.11,3x, &
-      &           ''|dE/dxyz| ='',F10.6)')j+1,edum,gsum     
-      do i=1,nat
-         write(jgr,'(3(F20.14,2x),4x,a2)')xyz(1,i),xyz(2,i),xyz(3,i), &
-         &                                toLcSymbol(iat(i))
-      enddo
-      do i=1,nat
-         write(jgr,'(3D22.13)')g(1,i),g(2,i),g(3,i)
-      enddo
-      a='$end'
-      write(jgr,'(a)')a
-      goto 24
-   endif
-   goto 21
-24 continue
+   read(igr,'(a)')a
+   write(jgr,'(''  cycle = '',i6,4x,''SCF energy ='',F18.11,3x, &
+   &           ''|dE/dxyz| ='',F10.6)')j+1,edum,gsum
+   do i=1,nat
+      write(jgr,'(3(F20.14,2x),4x,a2)')xyz(1,i),xyz(2,i),xyz(3,i), &
+      &                                toLcSymbol(iat(i))
+   enddo
+   do i=1,nat
+      write(jgr,'(3D22.13)')g(1,i),g(2,i),g(3,i)
+   enddo
+   a='$end'
+   write(jgr,'(a)')a
    close(igr)
    close(jgr)
    call execute_command_line('mv '//gtemp//' '//gfile)
