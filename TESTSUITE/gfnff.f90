a3110b3b48936c7c39238f287168ede3ff64e540
Marcel Stahn <70513124+MtoLStoN@users.noreply.github.com>
2020-11-27 14:58:33 +0100

    Reoptimized Water, added Methanol and Ethanol (#387)
    * Fixed problems with the optimization of large proteins.
    * Fixed problem with charged Compounds containing Phosphor.
    * Added ALPB Parameters for Methanol and Ethanol.
    * Fixed Testsuite.
    
    Signed-off-by: MtoLStoN <70513124+MtoLStoN@users.noreply.github.com>

--- TESTSUITE/gfnff.f90
+++ TESTSUITE/gfnff.f90
@@ -608,9 +608,9 @@ subroutine test_gfnff_sdf
    real(wp) :: energy, hl_gap, sigma(3, 3)
    real(wp), allocatable :: gradient(:, :)
    real(wp), parameter :: ref_energies(3) = &
-      &[-0.98330642628373_wp, -1.0769327949979_wp, -1.0770116012984_wp]
+      &[-0.98330642628373_wp, -1.0826001974262_wp, -1.0826319137928_wp]
    real(wp), parameter :: ref_gnorms(3) = &
-      &[0.11515550863614e-2_wp, 0.49692321804809e-2_wp, 0.49548841785655e-2_wp]
+      &[0.11515550863614e-2_wp, 0.59760221346330e-2_wp, 0.59645861418074e-2_wp]
 
    call init(env)
    do iMol = 1, 3
