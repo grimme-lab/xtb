7b3d90ce0d647fe638fc2e73611de318c8d6b95f
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-01-19 18:46:30 +0100

    Move IO related routines to separate module

--- src/freq/project.f90
+++ src/freq/project.f90
@@ -24,7 +24,7 @@ module xtb_freq_project
    implicit none
    private
 
-   public :: projectHessian
+   public :: projectHessian, trproj
 
 
 contains
@@ -146,5 +146,175 @@ subroutine projectHessian(hessian, mol, removeTrans, removeRot)
 
 end subroutine projectHessian
 
+subroutine trproj(natoms,nat3,xyz,hess,ldebug,nmode,mode,ndim)
+
+   !----------------------------------------------------------------------
+   !  subroutine trproj drives projection of hessian out of the
+   !  space of translational and rotational motions:
+   !  first get xyz c.m.; second get transl. and rot. projection matrix
+   !
+   !  get center of mass coordinates with unit mass
+   !
+   ! Input
+   !   natoms  = number of atoms
+   !   nat3    = 3*natoms
+   !   xyz     = cartesian coordinates
+   !       ldebug  = debug flag = .true. for debugging
+   !
+   ! Ouput fo gtrprojm.f
+   !   xyzucm  = temporary c.m. coordinates
+   !
+   !       hess        = projected hessian out of space of transl. and rot.
+   !                 motion
+   !
+   !----------------------------------------------------------------------
+
+   implicit none
+
+   ! Input
+   logical, intent(in) :: ldebug
+   integer, intent(in) :: natoms,nat3,nmode,ndim
+   real(wp), dimension(3,natoms) :: xyz
+   real(wp), dimension(nat3,ndim):: mode
+   ! Ouput
+   real(wp), dimension(nat3*(nat3+1)/2) :: hess
+   ! Local
+   integer :: i
+   real(wp) :: xm,ym,zm
+   real(wp), dimension(3,natoms) ::xyzucm
+
+   xyzucm(:,:) = xyz(:,:)
+
+   xm = 0.0_wp
+   ym = 0.0_wp
+   zm = 0.0_wp
+
+   do i=1,natoms
+      xm = xm + xyzucm(1,i)
+      ym = ym + xyzucm(2,i)
+      zm = zm + xyzucm(3,i)
+   end do
+
+   xm = xm/natoms
+   ym = ym/natoms
+   zm = zm/natoms
+
+
+   do i=1,natoms
+      xyzucm(1,i) = xyzucm(1,i) - xm
+      xyzucm(2,i) = xyzucm(2,i) - ym
+      xyzucm(3,i) = xyzucm(3,i) - zm
+   end do
+
+   ! get translational and rotational projection matrix
+
+   call gtrprojm(natoms,nat3,xyzucm,hess,ldebug,nmode,mode,ndim)
+
+end subroutine trproj
+
+!----------------------------------------------------------------------
+subroutine gtrprojm(natoms,nat3,xyzucm,hess,ldebug,nmode,mode,ndim)
+   !----------------------------------------------------------------------
+   ! calculating the translational-rotational projection matrix
+   !
+   ! Input
+   !   natoms  = number of atoms
+   !   nat3    = 3*natoms
+   !   xyzucm  = coords c.m. from gxyzucm.f
+   !           hess    = hessian
+   !       ldebug      = debug flag = .true. for debugging
+   !
+   ! Ouput
+   !   fmat    = F-matrix with translational and rotational vectors
+   !       pmat        = projection matrix P = (1-FFt)
+   !   hess    = projected hessian
+   !----------------------------------------------------------------------
+   use xtb_fixparam
+   use xtb_mctc_la, only : blckmgs,syprj
+
+   implicit none
+
+   ! Input
+   logical, intent(in) :: ldebug
+   integer, intent(in) :: natoms,nat3,nmode,ndim
+   real(wp), dimension(3,natoms) :: xyzucm
+   real(wp), dimension(nat3,ndim):: mode
+   ! Ouput
+   real(wp), dimension(nat3*(nat3+1)/2) :: hess
+
+   ! Local
+   integer :: i,ii,iii
+   real(wp), allocatable :: fmat(:,:)
+   integer :: nprj
+
+   nprj=6
+   if(nmode.gt.0) nprj=nprj+nmode
+   if(nmode.lt.0) nprj=nprj+fixset%n*3
+   allocate(fmat(nat3,nprj))
+   fmat(:,:) = 0.0_wp
+
+   if(nmode.ge.0) then
+      do i=1,natoms
+         do ii=1,3
+            !        translation vectors
+            fmat(3*(i-1)+ii,ii) = 1.0_wp
+         end do
+         !        rotational vectors
+         fmat(3*(i-1)+1,4) =  0.0_wp
+         fmat(3*(i-1)+2,4) = -xyzucm(3,i)
+         fmat(3*(i-1)+3,4) =  xyzucm(2,i)
+         fmat(3*(i-1)+1,5) =  xyzucm(3,i)
+         fmat(3*(i-1)+2,5) =  0.0_wp
+         fmat(3*(i-1)+3,5) = -xyzucm(1,i)
+         fmat(3*(i-1)+1,6) = -xyzucm(2,i)
+         fmat(3*(i-1)+2,6) =  xyzucm(1,i)
+         fmat(3*(i-1)+3,6) =  0.0_wp
+      end do
+   endif
+
+   if(nmode.gt.0) then  ! NMF
+      do i=1,nmode
+         fmat(1:nat3,6+i)=mode(1:nat3,i)
+      enddo
+   endif
+
+   if(nmode.lt.0) then ! exact fixing
+      do i=1,natoms
+         !        rotational vectors
+         fmat(3*(i-1)+1,1) =  0.0_wp
+         fmat(3*(i-1)+2,1) = -xyzucm(3,i)
+         fmat(3*(i-1)+3,1) =  xyzucm(2,i)
+         fmat(3*(i-1)+1,2) =  xyzucm(3,i)
+         fmat(3*(i-1)+2,2) =  0.0_wp
+         fmat(3*(i-1)+3,2) = -xyzucm(1,i)
+         fmat(3*(i-1)+1,3) = -xyzucm(2,i)
+         fmat(3*(i-1)+2,3) =  xyzucm(1,i)
+         fmat(3*(i-1)+3,3) =  0.0_wp
+      enddo
+      do i=1,fixset%n
+         iii=fixset%atoms(i)
+         do ii=1,3
+            fmat(3*(iii-1)+ii,3+(i-1)*3+ii) = 1.0_wp
+         end do
+      enddo
+   endif
+
+   if(ldebug) then
+      write(*,'(a)')
+      write(*,'(a)') ' Basis vectors before orthonormalization'
+      write(*,'(3e22.14)') fmat
+   end if
+
+   ! do orthogonalization
+
+   call  blckmgs(nat3,nprj,nat3,fmat)
+
+   ! do projection
+
+   call syprj(nat3,nprj,fmat,nat3,hess)
+
+   deallocate(fmat)
+
+end subroutine gtrprojm
 
 end module xtb_freq_project
