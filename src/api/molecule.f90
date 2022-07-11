c365ab7aca77757e53d740a53c933e83f9e43c3a
Ty <ty.balduf@schrodinger.com>
2022-07-11 18:00:56 -0400

    Export Symbols for MSVC
    By default, the Visual Studio C compiler doesn't export any symbols,
    so xtb.lib wasn't being created.
    Explicitly export C API functions using dllexport attribute.
    
    Signed-off-by: Ty <ty.balduf@schrodinger.com>

--- src/api/molecule.f90
+++ src/api/molecule.f90
@@ -45,6 +45,7 @@ contains
 function newMolecule_api(venv, natoms, numbers, positions, charge, uhf, lattice, &
       & periodic) result(vmol) &
       & bind(C, name="xtb_newMolecule")
+   !DEC$ ATTRIBUTES DLLEXPORT :: newMolecule_api
    character(len=*), parameter :: source = 'xtb_api_newMolecule'
    type(c_ptr), value :: venv
    type(VEnvironment), pointer :: env
@@ -95,6 +96,7 @@ end function newMolecule_api
 
 subroutine delMolecule_api(vmol) &
       & bind(C, name="xtb_delMolecule")
+   !DEC$ ATTRIBUTES DLLEXPORT :: delMolecule_api
    type(c_ptr), intent(inout) :: vmol
    type(VMolecule), pointer :: mol
 
@@ -111,6 +113,7 @@ end subroutine delMolecule_api
 
 subroutine updateMolecule_api(venv, vmol, positions, lattice) &
       & bind(C, name="xtb_updateMolecule")
+   !DEC$ ATTRIBUTES DLLEXPORT :: updateMolecule_api
    character(len=*), parameter :: source = 'xtb_api_updateMolecule'
    type(c_ptr), value :: venv
    type(VEnvironment), pointer :: env
