286a02df79ea3ca2788a5bdf82d79b71f942abb2
Igor S. Gerasimov <i.s.ger@ya.ru>
2022-11-23 20:51:56 +0900

    Computing of ESP on a given point for time estimation had incorrect index of array
    Signed-off-by: Igor S. Gerasimov <i.s.ger@ya.ru>

--- src/esp.f
+++ src/esp.f
@@ -124,7 +124,7 @@
       call timing(t0,w0)
       point=0
       call espints(n,nbf,xyz,intcut,point,pthr,P,
-     &             mprim,npp,nnn,indp,efact,gama,ee,dd,espe(i),basis)
+     &             mprim,npp,nnn,indp,efact,gama,ee,dd,espe(1),basis)
       call timing(t1,w1)
       write(*,'('' estimated wall time (s) '',f6.1)') np*(w1-w0)/nproc
       write(*,*)'computing ESP ...'
