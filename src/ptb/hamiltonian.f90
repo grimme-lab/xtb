191d244ddbe2dccd2089a5b648b0497b830bc135
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2023-11-12 15:17:43 +0100

    prepare H0 setup and remove unnecessary variables in tests
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- src/ptb/hamiltonian.f90
+++ src/ptb/hamiltonian.f90
@@ -22,6 +22,8 @@ module xtb_ptb_hamiltonian
    use mctc_env, only: wp, error_type
 
    use tblite_basis_type, only: basis_type
+   use tblite_adjlist, only: adjacency_list
+   use tblite_integral_multipole, only : msao
 
    use xtb_ptb_data, only: THamiltonianData
 
@@ -33,19 +35,30 @@ module xtb_ptb_hamiltonian
 
 contains
 
-   subroutine get_hamiltonian(mol, bas, overlap, overlap_h0, overlap_xc, vecp, hamiltonian)
+   subroutine get_hamiltonian(mol, list, bas, overlap, overlap_h0, overlap_xc, &
+      & vecp, selfenergies, hamiltonian)
       !> Molecular structure data
       type(structure_type), intent(in) :: mol
+      !> Neighbour list
+      type(adjacency_list), intent(in) :: list
       !> Basis set data
       type(basis_type), intent(in) :: bas
       !> (Scaled) overlap matrix
       real(wp), intent(in) :: overlap(:, :), overlap_h0(:, :), overlap_xc(:, :)
       !> Effective core potential
       real(wp), intent(in) :: vecp(:, :)
+      !> Self-energies
+      real(wp), intent(in) :: selfenergies(:)
       !> Effective Hamiltonian
       real(wp), allocatable, intent(out) :: hamiltonian(:, :)
+      !> H0 matrix
+      real(wp), allocatable :: h0(:, :)
 
-      allocate (hamiltonian(bas%nao, bas%nao))
+      allocate (hamiltonian(bas%nao, bas%nao), h0(bas%nao, bas%nao), source=0.0_wp)
+
+      hamiltonian = vecp
+
+      call get_h0(mol, list, bas, overlap_h0, selfenergies, h0)
 
    end subroutine get_hamiltonian
 
@@ -69,7 +82,7 @@ contains
 
       do iat = 1, mol%nat
          iid = mol%id(iat)
-         combinedcn = ( cn_normal(iat) + cn_star(iat) * hData%kcnstar(iid) )
+         combinedcn = (cn_normal(iat) + cn_star(iat)*hData%kcnstar(iid))
 
          !##### DEV WRITE #####
          ! write(*,*) 'cn_normal', cn_normal(iat)
@@ -82,8 +95,8 @@ contains
          ii = bas%ish_at(iat)
          do ish = 1, bas%nsh_id(iid)
             selfenergies(ii + ish) = hData%selfEnergy(ish, iid) + &
-               & hData%klh(ish,iid) * combinedcn + &
-               & cn_star(iat) * hData%kshift(iid)
+               & hData%klh(ish, iid)*combinedcn + &
+               & cn_star(iat)*hData%kshift(iid)
 
             !##### DEV WRITE #####
             ! write(*,*) 'selfenergies', ii + ish, selfenergies(ii + ish)
@@ -93,5 +106,66 @@ contains
       end do
 
    end subroutine get_selfenergy
+
+   subroutine get_h0(mol, list, bas, sh0, levels, h0mat)
+      !> Molecular structure data
+      type(structure_type), intent(in) :: mol
+      !> Neighbour list
+      type(adjacency_list), intent(in) :: list
+      !> Basis set data
+      type(basis_type), intent(in) :: bas
+      !> (Scaled) overlap matrix
+      real(wp), intent(in) :: sh0(:, :)
+      !> Levels
+      real(wp), intent(in) :: levels(:)
+      !> H0 matrix
+      real(wp), intent(out) :: h0mat(:, :)
+      !> Loop variables
+      integer :: iat, jat, izp, jzp, itr, k, img, inl
+      integer :: ish, jsh, is, js, ii, jj, iao, jao, nao, ij
+      !> Interatomic vector
+      real(wp) :: vec(3) = 0.0_wp
+      real(wp) :: r2
+
+      !> EXPLANATION:
+      !> Loop over all neighbours (iat =/= jat) and set the Hamiltonian for off-center elements
+      k=0
+      do iat = 1, mol%nat
+         write(*,*) "atom: ", iat, "id: ", mol%id(iat), "type: ", mol%num(mol%id(iat))
+         izp = mol%id(iat)
+         is = bas%ish_at(iat)
+         inl = list%inl(iat)
+         do img = 1, list%nnl(iat)
+            jat = list%nlat(img + inl)
+            write(*,*) "neighbour: ", jat, "id: ", mol%id(jat), "type: ", mol%num(mol%id(jat))
+            itr = list%nltr(img + inl)
+            jzp = mol%id(jat)
+            js = bas%ish_at(jat)
+            vec(:) = mol%xyz(:, iat) - mol%xyz(:, jat)
+            r2 = vec(1)**2 + vec(2)**2 + vec(3)**2
+            k = k + 1
+            write(*,*) "iteration: ", k, "distance: ", sqrt(r2)
+            do ish = 1, bas%nsh_id(izp)
+               ii = bas%iao_sh(is + ish)
+               do jsh = 1, bas%nsh_id(jzp)
+                  jj = bas%iao_sh(js + jsh)
+
+                  nao = msao(bas%cgto(jsh, jzp)%ang)
+                  do iao = 1, msao(bas%cgto(ish, izp)%ang)
+                     do jao = 1, nao
+                        ij = jao + nao*(iao - 1)
+                     end do
+                  end do
+
+               end do
+            end do
+
+         end do
+      end do
+
+      !> EXPLANATION:
+      !> Loop over all atoms and set the Hamiltonian for on-center elements
+
+   end subroutine get_h0
 
 end module xtb_ptb_hamiltonian
