7b4c8c57c27c47a846938c2932dcc753e56201a1
James H. Brown <james.h.brown@dartmouth.edu>
2024-06-11 12:25:59 -0400

    change product to hardcoded number fixes #1012 fixes #998 fixes #997
    Signed-off-by: James H. Brown <james.h.brown@dartmouth.edu>

--- src/gfnff/gfnff_eg.f90
+++ src/gfnff/gfnff_eg.f90
@@ -4039,9 +4039,9 @@ function get_cf(rTrans,gTrans,vol,avgAlp) result(cf)
   integer, parameter :: ewaldCutD(3) = 2
   integer, parameter :: ewaldCutR(3) = 2
   ! real space lattice vectors
-  real(wp), intent(in) :: rTrans( 3, product(2*ewaldCutD+1))
+  real(wp), intent(in) :: rTrans( 3, 125)
   ! reciprocal space lattice vectors
-  real(wp), intent(in) :: gTrans(3, product(2*ewaldCutR+1)-1)
+  real(wp), intent(in) :: gTrans(3, 124)
   ! unit cell volume 
   real(wp), intent(in) :: vol
   ! average alphaEEQ value
