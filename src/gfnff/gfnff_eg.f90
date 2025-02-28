d9be2525e19036b55da418cc39e8a6fda5694d72
Igor S. Gerasimov <i.s.ger@ya.ru>
2025-02-28 08:17:58 +0100

    Consistent zeroing
    Signed-off-by: Igor S. Gerasimov <i.s.ger@ya.ru>

--- src/gfnff/gfnff_eg.f90
+++ src/gfnff/gfnff_eg.f90
@@ -4142,9 +4142,7 @@ subroutine es_grad_sigma(mol, topo, nlist, rTrans, gTrans, xtmp, cf, &
    real(wp), allocatable :: amatdL(:, :, :)
    real(wp), allocatable :: atrace(:, :)
    allocate(amatdr(3,mol%n,mol%n), amatdL(3,3,mol%n), source = 0.0_wp)
-   allocate(atrace(3, mol%n))
-   amatdr = 0.0_wp
-   amatdL = 0.0_wp
+   allocate(atrace(3, mol%n), source = 0.0_wp)
 
    ! new routine from D4 for calculating derivatives
    call get_damat_3d(mol, topo, cf, xtmp, rTrans, gTrans, amatdr, amatdL, atrace)
