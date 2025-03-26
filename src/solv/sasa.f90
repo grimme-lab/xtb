8bcb4c4f4cf35fb0511e82bef8264ce6c811ca9a
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-03-26 23:10:29 +0100

    Drop tiny sasaij
    Signed-off-by: Igor S. Gerasimov <foxtranigor@gmail.com>

--- src/solv/sasa.f90
+++ src/solv/sasa.f90
@@ -183,7 +183,7 @@ pure subroutine compute_w_sp(nat,nnlists,trj2,vdwsa,xyza,nno,xyzp,sasap,grds, &
             ! sanity check for sasaij range
             sasaij = min(sasaij, 1.0_wp)
             sasaij = max(0.0_wp, sasaij)
-            if (sasaij == 0.0_wp) then
+            if (sasaij < tiny(0.0_wp)) then
                ! technically, we should not be here, but numerical world has its own opinion
                sasap = 0.0_wp
                return
