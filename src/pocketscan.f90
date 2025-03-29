a0400652b07a48779230ce88871bd36755e2f3c9
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-03-29 09:35:58 +0100

    Licen -> License
    Signed-off-by: Igor S. Gerasimov <foxtranigor@gmail.com>

--- src/pocketscan.f90
+++ src/pocketscan.f90
@@ -12,7 +12,7 @@
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU Lesser General Public License for more details.
 !
-! You should have received a copy of the GNU Lesser General Public Licen
+! You should have received a copy of the GNU Lesser General Public License
 ! along with xtb.  If not, see <https://www.gnu.org/licenses/>.
 
 !******************************************
